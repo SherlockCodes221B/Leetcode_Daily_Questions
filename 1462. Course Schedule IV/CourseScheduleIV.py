e907ee6cac12caac10cb2b0c5dd7114f8bc21e24
Sherlock Holmes <shreeshastudy@gmail.com>
2025-01-27 10:46:48 +0530

    27/01/2025

---
+++ 1462. Course Schedule IV/CourseScheduleIV.py
@@ -0,0 +1,57 @@
+'''
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course ai first if you want to take course bi.
+
+For example, the pair [0, 1] indicates that you have to take course 0 before you can take course 1.
+Prerequisites can also be indirect. If course a is a prerequisite of course b, and course b is a prerequisite of course c, then course a is a prerequisite of course c.
+
+You are also given an array queries where queries[j] = [uj, vj]. For the jth query, you should answer whether course uj is a prerequisite of course vj or not.
+
+Return a boolean array answer, where answer[j] is the answer to the jth query.
+
+ 
+
+Example 1:
+
+
+Input: numCourses = 2, prerequisites = [[1,0]], queries = [[0,1],[1,0]]
+Output: [false,true]
+Explanation: The pair [1, 0] indicates that you have to take course 1 before you can take course 0.
+Course 0 is not a prerequisite of course 1, but the opposite is true.
+Example 2:
+
+Input: numCourses = 2, prerequisites = [], queries = [[1,0],[0,1]]
+Output: [false,false]
+Explanation: There are no prerequisites, and each course is independent.
+Example 3:
+
+
+Input: numCourses = 3, prerequisites = [[1,2],[1,0],[2,0]], queries = [[1,0],[1,2]]
+Output: [true,true]
+
+
+'''
+
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        # Initialize the relation matrix
+        relation = [[False] * numCourses for _ in range(numCourses)]
+        print(relation)
+
+        # Fill in the direct prerequisites
+        for d in prerequisites:
+            relation[d[0]][d[1]] = True
+        print(relation)
+
+        # Perform Floyd-Warshall-like transitive closure
+        for i in range(numCourses):
+            for src in range(numCourses):
+                for target in range(numCourses):
+                    relation[src][target] = relation[src][target] or (relation[src][i] and relation[i][target])
+
+        # Answer the queries
+        ans = []
+        for d in queries:
+            ans.append(relation[d[0]][d[1]])
+
+        return ans
+                    
