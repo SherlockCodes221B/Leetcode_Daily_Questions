ca4a2898f0ee09e5f37900f06a822547190365fd
Sherlock Holmes <shreeshastudy@gmail.com>
2025-07-21 12:36:58 +0530

    7/21/2025

---
+++ 1957. Delete Characters to Make Fancy String/main.py
@@ -0,0 +1,39 @@
+'''
+A fancy string is a string where no three consecutive characters are equal.
+
+Given a string s, delete the minimum possible number of characters from s to make it fancy.
+
+Return the final string after the deletion. It can be shown that the answer will always be unique.
+
+ 
+
+Example 1:
+
+Input: s = "leeetcode"
+Output: "leetcode"
+Explanation:
+Remove an 'e' from the first group of 'e's to create "leetcode".
+No three consecutive characters are equal, so return "leetcode".
+Example 2:
+
+Input: s = "aaabaaaa"
+Output: "aabaa"
+Explanation:
+Remove an 'a' from the first group of 'a's to create "aabaaaa".
+Remove two 'a's from the second group of 'a's to create "aabaa".
+No three consecutive characters are equal, so return "aabaa".
+'''
+
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        ans = s[0]
+        cnt = 1
+        for i in range(1, len(s)):
+            if s[i] == ans[-1]:
+                cnt += 1
+                if cnt < 3:
+                    ans += s[i]
+            else:
+                cnt = 1
+                ans += s[i]
+        return ans
